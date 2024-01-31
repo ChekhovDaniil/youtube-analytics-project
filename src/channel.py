04962a26466accc51056588f196195e6f2b0a7e8
admin <scehovdaniil@gmail.com>
2024-01-31 14:49:31 +0300

    rework

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,27 @@
+import os
+
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(channel)
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API."""
+        pass
+
+    def to_json(self):
+        """Сохраняет в файл значения атрибутов экземпляра Channel."""
         pass
